23ce84388459ced1b235fde87e7e6371cd2f7de7
MAZTRO <design.mallet@gmail.com>
2020-03-05 22:24:21 -0500

    Add scripts for header

---
+++ scripts/header.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+    $(window).scroll(function()
+    {
+        if ($(window).scrollTop() > 50)
+        {
+            $("header").addClass("header_scroll");
+        }
+        else
+        {
+            $("header").removeClass("header_scroll");
+        }
+    });
+});
