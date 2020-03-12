17545b55938801cdb673003b44c0fd02867200ba
MAZTRO <designe.mallet@gmail.com>
2020-03-12 15:26:34 -0500

    Modify height of sections

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,58 +1,58 @@
 $(document).ready(function()
 {
-    //Click event
+    //Click event to go home section
     $("#HOME").click(function(evento)
     {
       //Go to px position slowly
-      $("html,body").animate({scrollTop:0}, "slow");
+      $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 
-    //Click event
+    //Click event to go About me section
     $("#ABOUT").click(function(evento)
     {
       //Go to px position slowly
-      $("html,body").animate({scrollTop:830}, "slow");
+      $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 
     //Click event
     $("#SKILLS").click(function(evento)
     {
       //Go to px position slowly
-      $("html,body").animate({scrollTop:1530}, "slow");
+      $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 
     //Click event
     $("#PORT").click(function(evento)
     {
       //Go to px position slowly
-      $("html,body").animate({scrollTop:2430}, "slow");
+      $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 
     //Click event
     $("#CONTACT").click(function(evento)
     {
       //Go to px position slowly
-      $("html,body").animate({scrollTop:3130}, "slow");
+      $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 
     //Click event
     $("#Logo").click(function(evento)
     {
       //Go to px position slowly
-      $("html,body").animate({scrollTop:0}, "slow");
+      $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 
     //Click event
     $("#DOWN").click(function(evento)
     {
     //Go to px position slowly
-    $("html,body").animate({scrollTop:830}, "slow");
+    $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 
     //Click event
     $("#UP").click(function(evento)
     {
     //Go to px position slowly
-    $("html,body").animate({scrollTop:0}, "slow");
+    $("html,body").animate({ scrollTop: $($(this).attr("href")).offset().top }, "slow");
     });
 });
