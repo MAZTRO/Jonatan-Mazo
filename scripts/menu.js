a04b46bc037db581900a757fdfa947076950e9e2
MAZTRO <design.mallet@gmail.com>
2020-08-03 12:52:00 -0500

    Add About section

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,13 +5,13 @@ const sleep = (milliseconds) => {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "20%";
-  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+  document.getElementById('Body').style.backgroundColor = "rgba(0, 0, 0, 0.4)";
   document.getElementById("hamburger").style.opacity = '0';
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-  sleep(250).then(() => {
+  sleep(150).then(() => {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
     document.getElementById("hamburger").style.opacity = '1';
--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -12,40 +12,26 @@ for (btn of btnsList) {
   btn.addEventListener('click', element => {
     const btnLength = element.path.length;
     console.log('Longitud:', btnLength);
+    console.log('Elemnt:', element);
 
-    if (btnLength === 9) {
-      console.log(element.path[0].className);
-      btnClass = element.path[0].className;
-    } else if (btnLength === 11) {
-      console.log(element.path[2].className);
-      btnClass = element.path[2].className;
-    } else {
+    if (btnLength === 9 && element.path[1].className === 'sidenav') {
+      console.log(element.path[0].innerText);
+      btnClass = element.path[0].innerText;
+    } else if (btnLength === 9) {
       console.log(element.path[1].className);
       btnClass = element.path[1].className;
+    } else {
+      console.log(element.path[0].className);
+      btnClass = element.path[0].className;
     }
 
-    if (btnClass === 'btnBanner') {
-      const anchor = document.querySelector('#banner');
+    if (btnClass === 'logo' || btnClass === 'Home') {
+      const anchor = document.querySelector('#cover');
       scroll.animateScroll(anchor);
     }
 
-    else if (btnClass === 'btnExperiences' || btnClass === 'arrow') {
-      const anchor = document.querySelector('#experiences');
-      scroll.animateScroll(anchor);
-    }
-
-    else if (btnClass === 'btnAbilities') {
-      const anchor = document.querySelector('#abilities');
-      scroll.animateScroll(anchor);
-    }
-
-    else if (btnClass === 'btnProject') {
-      const anchor = document.querySelector('#projects');
-      scroll.animateScroll(anchor);
-    }
-
-    else if (btnClass === 'btnContact') {
-      const anchor = document.querySelector('#contact');
+    else if (btnClass === 'About me' || btnClass === 'arrowD' || btnClass === 'ArrowDlink') {
+      const anchor = document.querySelector('#about');
       scroll.animateScroll(anchor);
     }
   });
