0e0155f61a7df93ad94e4c40608e9fef8fcddb05
MAZTRO <design.mallet@gmail.com>
2020-08-03 01:51:51 -0500

    Add a new schema of colors

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,19 @@
-var app = new Vue({
-  el: '#Menu',
-  data: {
-    banner: false,
-    experiences: false,
-    abilities: false,
-    project: false,
-    contact: false
-  }
-})
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "20%";
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+  document.getElementById("hamburger").style.opacity = '0';
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  sleep(250).then(() => {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("hamburger").style.opacity = '1';
+  })
+}
